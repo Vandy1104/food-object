dacd41ca2b8c7ebbda14a2781eb38ba4d1ee8c06
Vandy1104 <53416476+Vandy1104@users.noreply.github.com>
2019-10-22 15:26:06 +1300

    continue @ home

---
+++ Food-object-22.10.19/js/script.js
@@ -0,0 +1,123 @@
+console.log('Food Objects');
+
+var foods = [
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'wholeGrains',
+    rank:   2,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  },
+
+  {
+    name: 'greenVeggies',
+    rank:   1,
+    photo:'images/green.jpeg',
+    vegan: 'yes',
+    recipeName:   'Vegetables Salad',
+  }
+];
